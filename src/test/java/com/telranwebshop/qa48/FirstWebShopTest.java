fa9c606e3d0a3a98bb64fb7e8bf6bbc59a2b5298
TetianaKarakis <tetiana.karakis@gmail.com>
2025-08-21 13:07:19 +0300

    add tests  WebShop

--- src/test/java/com/telranwebshop/qa48/FirstWebShopTest.java
+++ src/test/java/com/telranwebshop/qa48/FirstWebShopTest.java
@@ -2,8 +2,11 @@ package com.telranwebshop.qa48;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class FirstWebShopTest {
     WebDriver driver;
@@ -11,11 +14,24 @@ public class FirstWebShopTest {
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
-        driver.get("https://demowebshop.tricentis.com");
+        //driver.get("https://demowebshop.tricentis.com");
+        driver.navigate().to("https://demowebshop.tricentis.com");
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.tel-ran.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.navigate().to("https://demowebshop.tricentis.com");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
     }
 
     @Test
     public void openDemoWebShop() {
         System.out.println("DemoWebShop opened");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
     }
 }
